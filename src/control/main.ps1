f643965513b958f693ee7a13df0ab3589374e809
joel <jolobartok1b@gmail.com>
2024-01-01 15:31:47 +0100

    start up

---
+++ src/control/main.ps1
@@ -0,0 +1,9 @@
+﻿while($true){
+    if(Get-Process -Name cmd){
+        Write-Host "process cmd exist"
+    } else {
+        Start-Process cmd -WindowStyle Maximized
+    }
+
+    Wait-Event -Timeout 15
+}
