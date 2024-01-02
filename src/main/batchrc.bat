54e8fc0ec0e02abbaf182f49fe444ae26c0a01c0
JoelBartok <JoloBartok1b@gmail.com>
2024-01-02 15:25:30 +0100

    optimization

--- src/main/batchrc.bat
+++ src/main/batchrc.bat
@@ -1,4 +1,5 @@
 @echo off
+title nex
 echo welcome.
 echo If you need to edit the start script, edit 'batchrc.bat'
 echo ========================================================
--- src/main/nex.ps1
+++ src/main/nex.ps1
@@ -1,15 +1,21 @@
 ﻿function start-cmd(){
     try {
+        # check the cmd process
         Get-Process -Name cmd -ErrorAction Stop
         Write-Host "process cmd exist"
     } catch {
-        Start-Process $command -WindowStyle Maximized
+        try {
+            # checks if batchrc.bat exists *
+            Start-Process $command -WindowStyle Maximized -ErrorAction Stop
+        } catch {
+            # otherwise run cmd instead of batchrc.bat *
+            $command = "cmd"
+            start-cmd
+        }   
     }
 }
 
 function start-check(){
-    start-cmd
-
     while($true){
         start-cmd
         Wait-Event -Timeout 10
@@ -17,10 +23,9 @@ function start-check(){
 }
 
 $command = "C:/Users/$env:username/batchrc.bat"
-if(test-path $command){
-    start-check   
-} else {
+if(!(test-path $command)){ 
     Write-Host "`a"
     $command = "cmd"
-    start-check    
-}
+} 
+
+start-check  
